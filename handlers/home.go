518d1e75f7f1fb708e66d5c2e5f81462d8fbb8a6
Ek <ekkapob@gmail.com>
2017-10-21 21:41:39 +0700

    init

--- handlers/home.go
+++ handlers/home.go
@@ -1,10 +1,11 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello</h1>")
+	t := template.Must(template.ParseFiles("templates/layout.html"))
+	t.Execute(w, nil)
 }
